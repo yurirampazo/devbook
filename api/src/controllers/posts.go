15b5bf1134d7e0862b4859dc21ac211bb3202a1e
yuri <ymrampazo@gmail.com>
2025-12-01 16:41:55 -0300

    feat(post): get post by id

--- api/src/controllers/posts.go
+++ api/src/controllers/posts.go
@@ -9,10 +9,13 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // Ads a new post in database
-func CreatePost(w http.ResponseWriter, r *http.Request){
+func CreatePost(w http.ResponseWriter, r *http.Request) {
 	userID, err := auth.ExtractUserID(r)
 	if err != nil {
 		response.Error(w, http.StatusUnauthorized, err)
@@ -52,25 +55,42 @@ func CreatePost(w http.ResponseWriter, r *http.Request){
 }
 
 // Gets the user feed user posts
-func FindPosts(w http.ResponseWriter, r *http.Request){
-
+func FindPosts(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // Finds an especific post
-func GetPostById(w http.ResponseWriter, r *http.Request){
+func GetPostById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	postID, err := strconv.ParseUint(params["id"], 10, 64)
+	if err != nil {
+		response.Error(w, http.StatusBadRequest, err)
+		return
+	}
 
+	db, err := database.Connect()
+	if err != nil {
+		response.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
 
+	repo := repository.NewFeedPostsRepository(db)
+	post, err := repo.GetById(postID)
+	if err != nil {
+		response.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	response.JSON(w, http.StatusOK, post)
 }
 
 // UPdate some post
-func EditPost(w http.ResponseWriter, r *http.Request){
-
+func EditPost(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // Delete some post
-func DeletePost(w http.ResponseWriter, r *http.Request){
-
+func DeletePost(w http.ResponseWriter, r *http.Request) {
 
 }
--- api/src/repository/feed_post.go
+++ api/src/repository/feed_post.go
@@ -33,3 +33,28 @@ func (repo FeedPost) Create(post model.FeedPost) (uint64, error) {
 	}
 	return uint64(lastInsertedID), nil
 }
+// Gets a post by its id
+func (repo FeedPost) GetById(id uint64) (model.FeedPost, error) {
+	line, err := repo.db.Query(`SELECT p.*, u.nick FROM posts p INNER JOIN users u ON u.id = p.author_id WHERE p.id = ?`, id)
+	if err != nil {
+		return model.FeedPost{}, err
+	}
+	defer line.Close()
+
+	var post model.FeedPost
+
+	if line.Next() {
+		if err = line.Scan(
+			&post.ID,
+			&post.Title,
+			&post.Content,
+			&post.AuthorID,
+			&post.Likes,
+			&post.CreatedAt,
+			&post.AuthorNick,
+		); err != nil {
+			return model.FeedPost{}, err
+		}
+	}
+	return post, nil
+}
